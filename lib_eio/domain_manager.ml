ff492837a0d6e42bb774e34606cd78abe92443d8
Lucas Pluvinage <lucas@tarides.com>
2023-06-08 15:16:30 +0200

    Name cancellation context and give names to internal fibers

--- lib_eio/domain_manager.ml
+++ lib_eio/domain_manager.ml
@@ -28,6 +28,7 @@ let run (Resource.T (t, ops)) fn =
   try
     Fiber.first
       (fun () ->
+         Ctf.set_name "eio.domain_mgr cancel thread";
          match Promise.await cancelled with
          | Cancel.Cancelled ex -> raise ex    (* To avoid [Cancelled (Cancelled ex))] *)
          | ex -> raise ex (* Shouldn't happen *)
