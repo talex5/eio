cdc4c2e6c99a4370f9737c2764c635f3aab91efc
Thomas Leonard <talex5@gmail.com>
2023-08-16 11:08:56 +0100

    Improve example

--- examples/fs/main.ml
+++ examples/fs/main.ml
@@ -4,16 +4,16 @@ let find ~output ~pattern dir =
   let pattern = Str.regexp_string_case_fold pattern in
   Eio.Buf_write.with_flow output @@ fun w ->
   let rec scan dir =
-    Eio.Path.read_dir dir |> List.iter (fun item ->
+    Eio.Path.read_dir dir |> List.iter (fun leaf ->
+        let item = dir / leaf in
+        let info = Eio.Path.stat item ~follow:false in
         begin
-          match Str.search_forward pattern item 0 with
+          match Str.search_forward pattern leaf 0 with
           | _ ->
-            Eio.Buf_write.string w item;
+            Eio.Buf_write.string w (Eio.Path.native_exn item);
             Eio.Buf_write.char w '\n';
           | exception Not_found -> ()
         end;
-        let item = dir / item in
-        let info = Eio.Path.stat item ~follow:false in
         if info.kind = `Directory then scan item
       )
   in
